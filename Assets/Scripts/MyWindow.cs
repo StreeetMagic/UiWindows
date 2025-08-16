273dccee5de91deee7845fd624e0147bcd3b12ab
senya <seniakuv99@gmail.com>
2025-08-16 16:02:38 +0300

    lil fix

--- Assets/Scripts/MyWindow.cs
+++ Assets/Scripts/MyWindow.cs
@@ -1,5 +1,17 @@
-﻿using UnityEngine.UI.Windows.WindowTypes;
+﻿using UnityEngine;
+using UnityEngine.UI.Windows.Components;
+using UnityEngine.UI.Windows.WindowTypes;
 
 public class MyWindow : LayoutWindowType
 {
+    private ButtonComponent button;
+
+    public override void OnInit()
+    {
+        GetLayoutComponent(out button);
+        base.OnInit();
+        
+        Debug.Log("работаем братья");
+        Debug.Log($"{button == null} пздц");
+    }
 }
